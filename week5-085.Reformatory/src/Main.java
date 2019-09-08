b760e749e0d2a8e87a060cdbad5d4919678c1b0c
Daniel Sepe Jr <dsepejr@gmail.com>
2019-09-07 23:21:34 -0400

    Exercise 85 Complete

--- week5-085.Reformatory/src/Main.java
+++ week5-085.Reformatory/src/Main.java
@@ -3,5 +3,27 @@ public class Main {
 
     public static void main(String[] args) {
         // write here test code 
+        Reformatory reform1 = new Reformatory();
+        
+        Person andy = new Person("Andy", 25, 180, 95);
+        Person carmen = new Person("Carmen", 27, 150, 75);
+        
+        // Calling weight method here
+        System.out.println(andy.getName() + " weight: " + reform1.weight(andy) + " kilos");
+        
+        reform1.feed(andy);
+        reform1.feed(andy);
+        // Calling weight method here #2
+        System.out.println(andy.getName() + " weight: " + reform1.weight(andy) + " kilos");
+        
+        // Calling weight method here #3
+        reform1.weight(andy);
+        System.out.println("total weights measured: " + reform1.totalWeightsMeasured());
+        
+        reform1.weight(carmen);
+        reform1.feed(carmen);
+        // Getting Carmen's weight here without calling the weight method
+        System.out.println(carmen.getName() + " weight: " + carmen.getWeight());
+        System.out.println("total weights measured: " + reform1.totalWeightsMeasured());
     }
 }
--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,20 @@
 public class Reformatory {
+    private int count = 0;
+    
 
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        this.count++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        this.count--; // Must negate the count here every time weight method is called by feed method
+        person.setWeight(this.weight(person) + 1);
+    }
+    
+    public int totalWeightsMeasured() {
+        return this.count;
     }
 
 }
