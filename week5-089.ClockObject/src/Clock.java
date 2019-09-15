6c5513e3d925297d6007ff5f5afd30c00461018f
Daniel Sepe Jr <dsepejr@gmail.com>
2019-09-15 00:47:19 -0400

    Exercise 89 in progress

--- week5-089.ClockObject/src/Clock.java
+++ week5-089.ClockObject/src/Clock.java
@@ -1,18 +1,29 @@
 public class Clock {
-    private BoundedCounter hours;
-    private BoundedCounter minutes;
-    private BoundedCounter seconds;
+    private BoundedCounter hours = new BoundedCounter(23);
+    private BoundedCounter minutes = new BoundedCounter(59);
+    private BoundedCounter seconds = new BoundedCounter(59);
    
     public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
-        // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+        // the counters that represent hours, minutes and seconds are created
+        // and set to have the correct initial values
+        
+        this.hours.setValue(hoursAtBeginning);  
+//        BoundedCounter hours = new BoundedCounter(23);
+//        BoundedCounter minutes = new BoundedCounter(59);
+//        BoundedCounter seconds = new BoundedCounter(59);
+//        this.hours = new BoundedCounter(hoursAtBeginning);
+        this.minutes.setValue(minutesAtBeginning);
+        this.seconds.setValue(secondsAtBeginning);
+        
     }
     
     public void tick() {
         // Clock advances by one second
+        this.seconds.next();
     }
     
     public String toString() {
         // returns the string representation
-        return "";
+        return this.hours + ":" + this.minutes + ":" + this.seconds;
     }
 }
--- week5-089.ClockObject/src/Main.java
+++ week5-089.ClockObject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Clock clock = new Clock(23, 59, 50);
 
         int i = 0;
-        while (i < 20) {
+        while (i < 121) {
             System.out.println(clock);
             clock.tick();
             i++;
