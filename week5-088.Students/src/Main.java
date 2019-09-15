a9a8d009a2add9aedc0487ece48101ba0118a6c7
Daniel Sepe Jr <dsepejr@gmail.com>
2019-09-14 21:20:55 -0400

    Exercise 88 Complete

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -5,6 +5,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write here the main program
+//        Student pekka = new Student("Pekka Mikkola", "013141590");
+//        System.out.println("name: " + pekka.getName());
+//        System.out.println("studentnumber: " + pekka.getStudentNumber());
+//        System.out.println( pekka );
+        Scanner reader = new Scanner(System.in);
+        
+        ArrayList<Student> list = new ArrayList<Student>();
+        
+        String name = " ";
+        String studentNumber = "";
+        System.out.print("name: ");
+        name = reader.nextLine();
+        
+        while (!name.equals("")) {
+            System.out.print("studentnumber: ");
+            studentNumber = reader.nextLine();
+            Student student = new Student(name, studentNumber);
+            list.add(student);
+            System.out.print("name: ");
+            name = reader.nextLine();
+        }
+        
+        System.out.println("");
+        
+        for ( Student student : list ) {
+            System.out.println( student );
+        }
+        
+        System.out.println("");
+        
+        System.out.print("Give search term: ");
+        String searched = reader.nextLine();
+        System.out.println("Result:");
+        for ( Student student : list ) {
+            if (student.getName().contains(searched)) {
+                System.out.println( student );
+            }
+        }
+        
     }
 }
