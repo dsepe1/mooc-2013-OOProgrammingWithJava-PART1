55b8a0b35041c91c2f1bfeafe35da2a871cbda08
Daniel Sepe Jr <dsepejr@gmail.com>
2019-10-04 20:28:58 -0400

    Exercise 95 Complete

--- week6-095.Money/src/Money.java
+++ week6-095.Money/src/Money.java
@@ -32,5 +32,42 @@ public class Money {
 
         return euros + "." + zero + cents + "e";
     }
+    
+    public Money plus(Money added) {
+        int initialTotal = this.euros * 100 + this.cents;
+        int addedTotal = added.euros * 100 + added.cents;
+        int centsTotal = initialTotal + addedTotal;
+        int newEurosTotal = centsTotal / 100;
+        int newCentsTotal = centsTotal % 100;
+        
+        Money actualTotal = new Money(newEurosTotal, newCentsTotal);
+                
+        return actualTotal;
+    }
+    
+    public boolean less(Money compared) {
+        int initialTotal = this.euros * 100 + this.cents;
+        int comparedTotal = compared.euros * 100 + compared.cents;
+        if (initialTotal < comparedTotal) {
+            return true;
+        }
+        return false;
+    }
+    
+    public Money minus(Money decremented) {
+        int initialTotal = this.euros * 100 + this.cents;
+        int decrementedTotal = decremented.euros * 100 + decremented.cents;
+        int difference = initialTotal - decrementedTotal;
+        int differenceEuros = difference / 100;
+        int differenceCents = difference % 100;
+        if (difference < 0) {
+            differenceEuros = 0;
+            differenceCents = 0;
+        }
+        
+        Money actualDifference = new Money(differenceEuros, differenceCents);
+        
+        return actualDifference;
+    }
 
 }
