6add675caa2c061fff2f6a8306529a0e8740c34c
Daniel Sepe Jr <dsepejr@gmail.com>
2019-10-09 18:02:37 -0400

    Exercise 100 Complete!
    Course developers, loved the redirect to "Richard Strauss - Also Sprach Zarathustra / 2001 Space Odyssey opening theme" on YouTube after checking for this exercise's completion! That was cool!

--- week6-100.NightSky/src/NightSky.java
+++ week6-100.NightSky/src/NightSky.java
@@ -14,17 +14,20 @@ public class NightSky {
     private double density;
     private int width;
     private int height;
+    private int starsInLastPrint;
     
     public NightSky(double density) {
-        this.density = density;
-        this.width = 20;
-        this.height = 10;
+//        this.density = density;
+//        this.width = 20;
+//        this.height = 10;
+        this(density, 20, 10);
     }
     
     public NightSky(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.density = 0.1;
+//        this.width = width;
+//        this.height = height;
+//        this.density = 0.1;
+        this(0.1, width, height);
     }
     
     public NightSky(double density, int width, int height) {
@@ -43,12 +46,25 @@ public class NightSky {
             // has to fall below... I guess that's one way of explaining it to myself.
             if (chance <= this.density) {
                 System.out.print("*");
+                this.starsInLastPrint++;
             } else {
                 System.out.print(" ");
             }
             chance = random.nextDouble();
         }
-        System.out.println("");
+//        System.out.println("");
+    }
+    
+    public void print() {
+        this.starsInLastPrint = 0;
+        for (int i = 0; i < this.height; i++) {
+            printLine();
+            System.out.println("");
+        }
+    }
+    
+    public int starsInLastPrint() {
+        return this.starsInLastPrint;
     }
     
 }
